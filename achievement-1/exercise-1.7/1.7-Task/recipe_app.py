2976f59d239386c645f210b082e6614319dadc04
Alexis Abner <alexisabner@alexiss-air.lan>
2024-08-24 05:24:30 -0600

    Add methods calculate difficulty and ingredients list

--- achievement-1/exercise-1.7/1.7-Task/recipe_app.py
+++ achievement-1/exercise-1.7/1.7-Task/recipe_app.py
@@ -8,6 +8,7 @@ Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
 
+
 class Recipe(Base):
     # Recipe (what can also be called a data model or a model), stores the representation 
     # of the table’s structure in the db.
@@ -20,11 +21,45 @@ class Recipe(Base):
     difficulty = Column(String(20))
 
     def __repr__(self):
-        return "<Recipe ID: " + str(self.id) + "-" + self.name + ">"
+        return "<Recipe ID: " + str(self.id) + "-" + self.name + " (Difficulty: " + self.difficulty + ")>"
+
+
+    def __str__(self):
+        recipe_str = (
+            f"\n{'-'*40}\n"
+            f"Recipe ID: {self.id}\n"
+            f"Name: {self.name}\n"
+            f"Ingredients: {self.ingredients}\n"
+            f"Cooking Time: {self.cooking_time} minutes\n"
+            f"Difficulty: {self.difficulty}\n"
+            f"{'-'*40}\n"
+        )
+        return recipe_str
+    
+
+    def calculate_difficulty(self):
+        ingredient_count = len(self.ingredients.split(', '))
+        
+        if self.cooking_time < 10 and ingredient_count < 4:
+            self.difficulty = "Easy"
+        elif self.cooking_time < 10 and ingredient_count >= 4:
+            self.difficulty = "Medium"
+        elif self.cooking_time >= 10 and ingredient_count < 4:
+            self.difficulty = "Intermediate"
+        elif self.cooking_time >= 10 and ingredient_count >= 4:
+            self.difficulty = "Hard"
+        else:
+            self.difficulty = "Difficulty Unknown"
+
+
+    def return_ingredients_as_list(self):
+        if self.ingredients == "":
+            return []
+        else:
+            return self.ingredients.split(', ')
     
 
     Base.metadata.create_all(engine)
-
 
 def main():
     print('todo')    
